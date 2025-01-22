b8b46ba9d585a108cb46db92e169c6429feb73a9
AayushShah-904 <aayushshah90421@gmail.com>
2025-01-22 09:35:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+interface HotelBuiler
+{
+    void taste();
+    void dessert();
+    void drink();
+}
+
+class PunjabiBuilder implements HotelBuiler
+{
+    
+}
+
+public class Main {
+    public static void main(String args[])
+    {
+
+    }
+}
