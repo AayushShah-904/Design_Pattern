04aa62e1f97798deaefe37ded0f08fde6511ba2d
AayushShah-904 <aayushshah90421@gmail.com>
2025-02-17 09:40:09 +0530

    Update Adapter_Design_Pattern.java

--- Adapter_Design_Pattern.java
+++ Adapter_Design_Pattern.java
@@ -1,79 +1,73 @@
-interface Target
-{
+import java.util.Scanner;
+
+interface Target {
     public double getPriceinUSD();
     public void setPriceinUSD(double setPriceinUSD);
     public double getPriceinINR();
     public void setPriceinINR(double setPriceinINR);
 }
 
-
-class Adaptee
-{
+// USD
+class Adaptee {
     double price;
-    public Adaptee(){};
 
-    public double getPrice()
-    {
+    public Adaptee() {}
+
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price)
-    {
-        this.price=price;
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
 
-class Adapter extends Adaptee implements Target
-{
-    public double getPriceinUSD()
-    {
+// Convert USD to INR
+class Adapter extends Adaptee implements Target {
+    public double getPriceinUSD() {
         return getPrice();
     }
 
-    public void setPriceinUSD(double price)
-    {
+    public void setPriceinUSD(double price) {
         setPrice(price);
     }
 
-    public double getPriceinINR()
-    {
+    public double getPriceinINR() {
         return USDtoINR(getPrice());
     }
 
-    public void setPriceinINR(double setPriceinINR)
-    {
+    public void setPriceinINR(double setPriceinINR) {
         setPrice(INRtoUSD(setPriceinINR));
     }
-    
-    private double USDtoINR(double USD)
-    {
-        return USD*86.7;
+
+    private double USDtoINR(double USD) {
+        return USD * 86.7; // Conversion rate
     }
 
-    private double INRtoUSD(double INR)
-    {
-        return INR/86.7;
+    private double INRtoUSD(double INR) {
+        return INR / 86.7; // Conversion rate
     }
-
-
 }
 
-public class Adapter_Design_Pattern
-{
-    public static void main(String args[])
-    {
+public class Adapter_Design_Pattern {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
         Target adapter = new Adapter();
 
-        adapter.setPriceinUSD(100);
+        // Input price in USD
+        System.out.print("Enter price in USD: ");
+        double priceInUSD = scanner.nextDouble();
+        adapter.setPriceinUSD(priceInUSD);
         System.out.println("Price in USD: " + adapter.getPriceinUSD());
         System.out.println("Price in INR: " + adapter.getPriceinINR());
 
-        adapter.setPriceinINR(500);
+        // Input price in INR
+        System.out.print("Enter price in INR: ");
+        double priceInINR = scanner.nextDouble();
+        adapter.setPriceinINR(priceInINR);
         System.out.println("Price in USD: " + adapter.getPriceinUSD());
         System.out.println("Price in INR: " + adapter.getPriceinINR());
-        
-      
 
+        scanner.close();
     }
-
 }
